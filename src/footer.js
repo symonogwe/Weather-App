04daefb1a62084edf02b4ca2194be77968ea842b
Symon Ogwe <symonogwe@gmail.com>
2023-10-10 08:45:31 +0300

    Create footer

---
+++ src/footer.js
@@ -0,0 +1,8 @@
+import githubSvg from "./Assets/github.svg";
+import twitterSvg from "./Assets/twitter.svg";
+
+const githubImg = document.querySelector(".github-img");
+githubImg.src = githubSvg;
+
+const twitterImg = document.querySelector(".twitter-img");
+twitterImg.src = twitterSvg;
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import "./Styles/styles.css";
 import * as leftWeatherComponents from "./left-weather";
 import * as rightWeatherComponents from "./right-weather";
 import * as domComponents from "./domAssignment";
+import * as footerComponents from "./footer";
